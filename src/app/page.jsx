fb0c06d706ea5b5a45700c9f08815db6a98285a3
nathanieluriri <uririnathaniel@gmail.com>
2025-03-11 15:52:48 +0100

    added more styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -26,6 +26,8 @@ export default function Home() {
     <>
       <HeroSection />
       <Padding />
+      <Padding />
+      <Padding />
       <AboutSection />
       <FeaturesSection />
       <Padding />
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -11,18 +11,18 @@ export function MenuButton() {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="ghost" className="md:hidden text-white hover:bg-transparent">
+        <Button variant="ghost" className="md:hidden text-white text-5xl hover:bg-transparent">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            className="w-6 h-6"
+            className="w-15 h-15"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              strokeWidth="2"
+              strokeWidth="3"
               d="M4 6h16M4 12h16M4 18h16"
             />
           </svg>
