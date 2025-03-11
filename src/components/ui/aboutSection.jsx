689f10593a49c4eeab6dbfbdacc67d56ba4ef769
nathanieluriri <uririnathaniel@gmail.com>
2025-03-11 20:44:44 +0100

    added a better sizing for the pixel

--- src/components/ui/aboutSection.jsx
+++ src/components/ui/aboutSection.jsx
@@ -4,10 +4,10 @@
 export function AboutSection() {
   return (
    <>
-    <section className="p-3 pt-22 md:pt-6 h-fit max-h-[550] max-w-[1600px] w-screen flex items-center justify-center  bg-cover bg-center bg-no-repeat"  style={{
+    <section className="p-3 mt-[130px] pt-22 md:pt-6 h-fit max-h-[550] max-w-[1600px] w-screen flex items-center justify-center  bg-cover bg-center bg-no-repeat"  style={{
         backgroundImage: "url('/background_for_about_section.png')",
       }}>
-      <div className=" text-left sm:text-center font-semibold max-w-[800px]">
+      <div className="p-6 text-left sm:text-center font-semibold max-w-[800px]">
         <p style={{ color: "#FFB204" }} className="text-sm pl-3 ">
           Earn & Learn on Delux{" "}
         </p>
