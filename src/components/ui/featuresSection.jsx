610d6d9513f670803600e4b00c0eb0592965bb14
nathanieluriri <uririnathaniel@gmail.com>
2025-03-17 08:46:51 +0100

    updated feature section

--- src/components/ui/featuresSection.jsx
+++ src/components/ui/featuresSection.jsx
@@ -3,20 +3,26 @@ import { CloudUpload } from "lucide-react";
 import { Lock } from "lucide-react";
 import { RefreshCcw } from "lucide-react";
 import { Clapperboard } from "lucide-react";
+import { FaTiktok } from "react-icons/fa";
+import { FaMoneyBill } from "react-icons/fa";
+import { FaGamepad } from "react-icons/fa";
+
 export function FeaturesGrid() {
   return (
     <>
       <div className="flex items-start gap-4 mt-10">
         <div className=" bg-amber-300 p-3 rounded-full">
-          <CloudUpload color="white" />
+          <FaTiktok color="white" />
         </div>
         <div className="flex-col flex gap-2">
-          <p className="font-bold text-lg">Skill Acquisition</p>
+          <p className="font-bold text-lg">TikTok Monetisation 👇🏽👇🏽</p>
           <p className="text-base/7 text-gray-600 ">
-            Delux isn’t just about earning; it’s also about learning. Our
-            platform offers users access to expert-led courses in various areas
-            of the digital world, helping you develop new skills and unlock your
-            smartphone’s full potential.
+            On Delux, our users can earn extra income from engaging on our
+            content and reposting it on TikTok. After reposting, users can
+            screenshot and attach their social media handles to gain access to
+            regular payments and lots of rewards for Social Media engagement....
+            In this way, users can earn up to 100 euros in a week
+            from their TikTok.
           </p>
         </div>
       </div>
@@ -25,12 +31,13 @@ export function FeaturesGrid() {
           <Lock color="white" />
         </div>
         <div className="flex-col flex gap-2">
-          <p className="font-bold text-lg">Quick Loans</p>
+          <p className="font-bold text-lg">Delux Ajo feature 👇🏽👇🏽 </p>
           <p className="text-base/7 text-gray-600 ">
-            With Delux Loan, users can request a loan without needing any
-            collateral and at a very fair interest rate. This feature is
-            available to all Delux users and is designed to help small business
-            owners grow by providing quick financial support.
+            Need urgent money to sort out a need?? Delux got you covered 💯.
+            Users on Delux, can join small groups with fellow members,
+            contribute weekly amounts from daily job earnings, and withdraw the
+            amounts contributed, turn by turn... Delux helps you & your finances
+            in EVERY single way 📌
           </p>
         </div>
       </div>
@@ -39,12 +46,13 @@ export function FeaturesGrid() {
           <RefreshCcw color="white" />
         </div>
         <div className="flex-col flex gap-2">
-          <p className="font-bold text-lg">Social Media Monetization</p>
+          <p className="font-bold text-lg">Delux AI Feature 👇🏽👇🏽</p>
           <p className="text-base/7 text-gray-600 ">
-            Delux allows users to monetize their social media accounts
-            effortlessly. By connecting your Instagram or TikTok account to
-            Delux, you can create videos showcasing Delux and its features.
-            Simply use our official hashtags and tag us in your posts.
+            This is the amazing Feature on Delux that makes your work as a
+            Coach/influencer easy 🌝. From answering any questions or confusions
+            new users have, to chatting for assistance, to even helping out in
+            doing daily jobs, Delux AI is the perfect Assistant.... On Delux, we
+            embrace innovation and smart work 🧡
           </p>
         </div>
       </div>
@@ -53,13 +61,49 @@ export function FeaturesGrid() {
           <Clapperboard color="white" />
         </div>
         <div className="flex-col flex gap-2">
-          <p className="font-bold text-lg">Delux Movies & Clips</p>
+          <p className="font-bold text-lg">Manus Earnings Feature 👇🏽👇🏽</p>
           <p className="text-base/7 text-gray-600 ">
-            Watch Delux Movies & Clips daily to earn rewards. Each movie or clip
-            you watch earns you a payout, which is added to your affiliate
-            balance. New content is posted every day, giving you consistent
-            chances to boost your earnings just by enjoying great films and
-            videos.
+            On Delux, we reward consistent Coaches who duplicate their effort in
+            their team members. As you create more millionaires in your downline
+            team, you earn extra cash for your effort, and also qualify for
+            various incentives 🎉🎉 Diverge Into Millions With Delux 
+          </p>
+        </div>
+      </div>
+
+     
+
+      <div className="flex items-start gap-4 mt-10">
+        <div className=" bg-amber-300 p-3 rounded-full">
+          <Clapperboard color="white" />
+        </div>
+        <div className="flex-col flex gap-2">
+          <p className="font-bold text-lg">TikTok Monetisation 👇🏽👇🏽</p>
+          <p className="text-base/7 text-gray-600 ">
+            On Delux, our users can earn extra income from engaging on our
+            content and reposting it on TikTok. After reposting, users can
+            screenshot and attach their social media handles to gain access to
+            regular payments and lots of rewards for Social Media engagement....
+            In this way, users can earn up to 100 euros in a week
+            from their TikTok.
+          </p>
+        </div>
+      </div>
+
+
+      <div className="flex items-start gap-4 mt-10">
+        <div className=" bg-amber-300 p-3 rounded-full">
+          <Clapperboard color="white" />
+        </div>
+        <div className="flex-col flex gap-2">
+          <p className="font-bold text-lg">Delux Movie Clips Feature 👇🏽👇🏽</p>
+          <p className="text-base/7 text-gray-600 ">
+            This is an amazing Feature on Delux, where users can take a break
+            from their daily Delux jobs, to watch short movie clips and earn in
+            Euros. There would be multiple Delux clips for uses to watch, engage
+            and share, during their free time to earn extra cash daily. Earnings
+            from Delux clips are added up, and can be withdrawn regularly.... ON
+            DELUX, YOU DON'T JUST EARN, YOU HAVE FUN WHILE DOING IT!!!
           </p>
         </div>
       </div>
