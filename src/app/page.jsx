381d833aeb819439b64bdf7a067aef5c677ee138
nathanieluriri <uririnathaniel@gmail.com>
2025-03-24 14:27:03 +0100

    commented some sections

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,14 @@
 "use client";
-import { FeaturesGrid } from "@/components/ui/featuresSection";
+// import { FeaturesGrid } from "@/components/ui/featuresSection";
 import { HeroSection } from "@/components/ui/heroSection";
-import { FootMenu } from "@/components/ui/footMenu";
+// import { FootMenu } from "@/components/ui/footMenu";
 import { Padding } from "@/components/ui/padding";
 import { Navbar } from "@/components/ui/Navbar";
 import { useOverlay } from "@/components/ui/overlayComponent";
-import { AboutSection } from "@/components/ui/aboutSection";
+// import { AboutSection } from "@/components/ui/aboutSection";
 import { ContactSectionComponent } from "@/components/ui/contactSection";
 import { RankingSection } from "@/components/ui/rankingSection";
-import { AdditionalInfo } from "@/components/ui/additionalInfo";
+// import { AdditionalInfo } from "@/components/ui/additionalInfo";
 import { AjoSection } from "@/components/ui/ajoSection";
 
 export default function Home() {
@@ -19,39 +19,39 @@ export default function Home() {
       <OverlayContent isOpen={isOpen} onClose={closeOverlay} />
       <HeroSection />
 
-      <AboutSection />
-      <div className="sm:hidden">
+      {/* <AboutSection /> */}
+      {/* <div className="sm:hidden">
         <Padding />
         <Padding />
         <Padding />
         <Padding />
         <Padding />
-      </div>
-      <FeaturesSection />
+      </div> */}
+      {/* <FeaturesSection /> */}
+      {/* <Padding />
       <Padding />
-      <Padding />
-      <Padding />
+      <Padding /> */}
       <ContactSection />
       <RankingSection />
-      <AdditionalInfo />
+      {/* <AdditionalInfo /> */}
       <Padding />
       <AjoSection />
       <Padding />
-      <FootMenu />
+      {/* <FootMenu /> */}
     </>
   );
 }
 
-function FeaturesSection() {
-  return (
-    <section
-      id="feature-section"
-      className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 text-left mx-auto max-w-2xl sm:mt-14 lg:mt-10 lg:max-w-4xl p-3"
-    >
-      <FeaturesGrid />
-    </section>
-  );
-}
+// function FeaturesSection() {
+//   return (
+//     <section
+//       id="feature-section"
+//       className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 text-left mx-auto max-w-2xl sm:mt-14 lg:mt-10 lg:max-w-4xl p-3"
+//     >
+//       <FeaturesGrid />
+//     </section>
+//   );
+// }
 
 function ContactSection() {
   return (
